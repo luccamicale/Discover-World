2d34f29354e166a16294d530ad2a6eacee613da9
luccamicale <luccamicale2015@gmail.com>
2023-11-20 11:49:32 -0300

    feat: :art: I modified the way to connect the information with the localhost

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
 // app.js
 const express = require('express');
-const dbconnect = require('./config');
-const City = require('./city'); // Importa el modelo City
+const fs = require('fs'); // Nueva línea para manejar archivos
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Las mejores 20 ciudades del mundo');
+  res.send('¡Las mejores 20 ciudades del mundo!');
 });
 
-app.get('/cities', async (req, res) => {
+app.get('/cities', (req, res) => {
   try {
-    const cities = await City.find({}, '_id name');
-    res.json(cities);
+    const rawData = fs.readFileSync('db.json');
+    const cities = JSON.parse(rawData);
+    const cityNames = cities.map(city => ({ name: city.name }));
+    res.json(cityNames);
   } catch (error) {
-    console.error('Error al obtener datos de la base de datos:', error.message);
+    console.error('Error al obtener datos del archivo JSON:', error.message);
     res.status(500).send('Error interno del servidor');
   }
 });
 
-app.get('/cities/:cityId', async (req, res) => {
-  const cityId = req.params.cityId;
+app.get('/cities/:cityName', (req, res) => {
+  const cityName = req.params.cityName;
 
   try {
-    const city = await City.findById(cityId);
+    const rawData = fs.readFileSync('db.json');
+    const cities = JSON.parse(rawData);
+
+    const city = cities.find(city => city.name === cityName);
+
     if (!city) {
       return res.status(404).json({ mensaje: 'Ciudad no encontrada' });
     }
 
-    res.json(city);
+    res.json(city.attractions);
   } catch (error) {
-    console.error('Error al obtener detalles de la ciudad:', error.message);
+    console.error('Error al obtener detalles de la ciudad desde el archivo JSON:', error.message);
     res.status(500).send('Error interno del servidor');
   }
 });
 
-
-
-app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-dbconnect();
--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const dbconnect = async () => {
-    mongoose.set('strictQuery', true)
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/cities", {});
-        console.log('Conexión exitosa a la base de datos');
-    } catch (error) {
-        console.error('Error al conectar a la base de datos:', error.message);
-    }
-}
+// const dbconnect = async () => {
+//     mongoose.set('strictQuery', true)
+//     try {
+//         await mongoose.connect("mongodb://127.0.0.1:27017/cities", {});
+//         console.log('Conexión exitosa a la base de datos');
+//     } catch (error) {
+//         console.error('Error al conectar a la base de datos:', error.message);
+//     }
+// }
 
-module.exports = dbconnect;
+// module.exports = dbconnect;
